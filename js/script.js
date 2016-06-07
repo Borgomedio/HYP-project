1fc85958b221a1e3b2161a1c5cc4e39ff992f253
Andrea <andrea.borgotti@gmail.com>
2016-06-07 11:45:32 +0200

    tentativoPHP

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var GIndex=1;
 
 $(document).ready(Ready);
+$(document).ready(deviceDL);
 
 function Ready(){
 
@@ -26,3 +27,40 @@ function animateGallery() {
 
 }
 
+
+
+function deviceDL(){
+    console.log("I'm ready!");
+    var id=1;
+    
+    $.ajax({
+        method: "POST",
+        crossDomain: true, 
+        url: "php/conDBdv-promotions.php", 
+        data: {device:id},
+        success: function(response) {
+            console.log(JSON.parse(response));
+            var device=JSON.parse(response);
+            var el="";
+            for(var i=0;i<device.length;i++){
+                console.log(device[i].title);
+                
+                el+=
+                "<div class='col-md-4 portfolio-item'> 
+                    <a href='#'> <img class='img-responsive' src='"+device[i].G+"' alt=''></a>
+                    <h3> <a href="#">"+device[i].A+"</a> </h3>
+                    <p>"+device[i].D+"</p>
+                    <a href='#' class='btn btn-primary' role='button'>View More</a>
+                </div>";             
+                
+            }
+            
+            $(".row").html(el);
+        },
+        error: function(request,error) 
+        {
+            console.log("Error");
+        }
+    });
+
+}
