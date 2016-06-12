6796c84e9704944e1350d453f6a51c9d170307cc
Andrea Borgotti <andrea.borgotti@gmail.com>
2016-06-12 15:26:11 +0200

    tentativo di far funzionare pag device singolo
    non ci sono riuscito. chiederò al toutoring

--- js/dv-promotions.js
+++ js/dv-promotions.js
@@ -13,9 +13,9 @@ function deviceDL(){
             var device=JSON.parse(response);
             var el="";
             for(var i=0;i<device.length;i++){
-                console.log(device[i].title);
+                console.log(device[i].A);
                 
-                el+= "<div class='col-md-4 text-center  portfolio-item' id='dvPRbl'>  <a href='#'> <img class='img-responsive center-block' src='"+device[i].G+"' alt=''></a> <h5> <a href='#'>"+device[i].A+"</a> </h5><p class='prezzo''>"+device[i].D+"</p><a href='#' class='btn btn-primary' role='button'>View More</a></div>";             
+                el+= "<div class='col-md-4 text-center  portfolio-item'>  <a href='#'> <img class='img-responsive center-block' src='"+device[i].G+"' alt=''></a> <h5> <a href='#'>"+device[i].A+"</a> </h5><p class='prezzo''>"+device[i].D+"</p><a href='#' class='btn btn-primary' role='button'>View More</a></div>";             
                 
             }
             
--- php/conDBdv.php
+++ php/conDBdv.php
@@ -1,23 +1,23 @@
 <?php
     
-    $mysqli = new mysqli("localhost", "root", "", "ODS_DB");
+    $mysqli = new mysqli("localhost", "root", "", "ods_db");
 
     if (mysqli_connect_errno()) { 
         echo "Error to connect to DBMS: ".mysqli_connect_error(); 
         exit();  
     }
     else {
-        $query = " SELECT A,B,C,D,G FROM Device WHERE E=1";
+        $queryy = " SELECT * FROM device ";
     
-        $risultato = $mysqli->query($query);
+        $risultato = $mysqli->query($queryy);
     
         if($risultato->num_rows >0)
         {
-            $myArray = array();
+            $myArrayy = array();
             while($row = $risultato->fetch_array(MYSQL_ASSOC)) {
-                $myArray[] = $row;
+                $myArrayy[] = $row;
             }
-            echo json_encode($myArray);
+            echo json_encode($myArrayy);
         }
 
         $risultato->close();
